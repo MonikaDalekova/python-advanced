84719c1a0831fa0ebf182b79e5d7156f0d9b614a
Monika Hristova <156455305+MonikaDalekova@users.noreply.github.com>
2024-04-22 12:08:11 +0300

    Update 06_battle_of_names.py

--- 02_tuples_and_sets/06_battle_of_names.py
+++ 02_tuples_and_sets/06_battle_of_names.py
@@ -4,10 +4,7 @@ set_odd = set()
 for row in range(1, n+1):
     name = input()
     result = sum([int(ord(letter))for letter in name]) // row
-    if result % 2 == 0:
-        set_even.add(result)
-    else:
-        set_odd.add(result)
+    set_even.add(result) if result % 2 == 0 else set_odd.add(result)
 sum_set_even = sum(set_even)
 sum_set_odd = sum(set_odd)
 if sum_set_odd == sum_set_even:
