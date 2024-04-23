c597d82830317418404c5987ff0d91bee6ea84d6
Monika Hristova <156455305+MonikaDalekova@users.noreply.github.com>
2024-04-23 15:47:23 +0300

    Update 02_expression_evaluator.py

--- 03_tuples_sets_stacks_queues_excercise/02_expression_evaluator.py
+++ 03_tuples_sets_stacks_queues_excercise/02_expression_evaluator.py
@@ -23,3 +23,27 @@ while index < len(expression):
         index = 1
     index += 1
 print(floor(int(expression[0])))
+
+#2
+# from collections import deque
+# from functools import reduce
+# from math import floor
+# 
+# expression = deque(input().split())
+# index = 0
+# 
+# functions = {
+#     "*": lambda i: reduce(lambda a, b: a*b, map(int, expression[:i])),
+#     "+": lambda i: reduce(lambda a, b: a+b, map(int, expression[:i])),
+#     "-": lambda i: reduce(lambda a, b: a-b, map(int, expression[:i])),
+#     "/": lambda i: reduce(lambda a, b: a/b, map(int, expression[:i]))
+# }
+# 
+# while index < len(expression):
+#     element = expression[index]
+#     if element in "*+-/":
+#         expression[0] = functions[element](index)
+#         [expression.pop(1) for i in range(index)]
+#         index = 1
+#     index += 1
+# print(floor(int(expression[0])))
