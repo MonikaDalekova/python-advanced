b4f61607c37ef0d770261bd659fe474dbb305224
Monika Hristova <156455305+MonikaDalekova@users.noreply.github.com>
2024-04-25 13:36:02 +0300

    Add files via upload

---
+++ 04_multidimensional_lists/02_even_matrix.py
@@ -0,0 +1,11 @@
+matrix = [[int(el) for el in input().split(", ") if int(el) % 2 == 0] for el in range(int(input()))]
+
+print(matrix)
+
+#2
+# row = int(input())
+# matrix = []
+# for i in range(row):
+#     inner_list = [int(el) for el in input.split(", ")]
+#     matrix.append(inner_list)
+# print(matrix)
