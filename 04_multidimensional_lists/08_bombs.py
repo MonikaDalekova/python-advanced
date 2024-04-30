864e6ccba989dfc465bc52fd64b195878b12b4a8
Monika Hristova <156455305+MonikaDalekova@users.noreply.github.com>
2024-04-30 14:07:46 +0300

    Update 08_bombs.py

--- 04_multidimensional_lists/08_bombs.py
+++ 04_multidimensional_lists/08_bombs.py
@@ -1,3 +1,4 @@
+#1
 size = int(input())
 matrix = [[int(x) for x in input().split()] for el in range(size)]
 bomb_coordinates = ((int(x) for x in el.split(',')) for el in input().split()) # правим го в tuple, защото няма да ги променяме
@@ -22,6 +23,32 @@ for row, col in bomb_coordinates:
 
         if 0 <= r < size and 0 <= c < size:
             matrix[r][c] -= matrix[row][col] if matrix[r][c] > 0 else 0\
+
+alive_cells = [num for row in range(size) for num in matrix[row] if num > 0]
+
+print(f"Alive cells: {len(alive_cells)}")
+print(f"Sum: {sum(alive_cells)}")
+
+[print(*row) for row in matrix]
+
+#2
+size = int(input())
+matrix = [[int(x) for x in input().split()] for el in range(size)]
+bomb_coordinates = ((int(x) for x in el.split(',')) for el in input().split()) # правим го в tuple, защото няма да ги променяме
+
+
+for row, col in bomb_coordinates:
+    bomb_value = matrix[row][col]
+
+    if bomb_value <= 0:
+        continue
+
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            r, c = row + x, col + y
+
+            if 0 <= r < size and 0 <= c < size:
+                matrix[r][c] -= bomb_value if matrix[r][c] > 0 else 0
 
 alive_cells = [num for row in range(size) for num in matrix[row] if num > 0]
 
