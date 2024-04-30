87e091e5a5841f27d1b14b5086d0393de8eda7c6
Monika Hristova <156455305+MonikaDalekova@users.noreply.github.com>
2024-04-30 14:01:00 +0300

    Add files via upload

---
+++ 04_multidimensional_lists/08_bombs.py
@@ -0,0 +1,31 @@
+size = int(input())
+matrix = [[int(x) for x in input().split()] for el in range(size)]
+bomb_coordinates = ((int(x) for x in el.split(',')) for el in input().split()) # правим го в tuple, защото няма да ги променяме
+
+directions = (
+    (-1, 0), # up
+    (1, 0), # down
+    (0, -1), # left
+    (0, 1), # right
+    (-1, 1), # top right
+    (-1, -1), # top left
+    (1, -1), # down left
+    (1, 1), # down right
+    (0, 0) # current
+)
+for row, col in bomb_coordinates:
+    if matrix[row][col] <= 0:
+        continue
+
+    for x, y in directions: #определяме кои са цифрите на тези координати
+        r, c = row + x, col + y
+
+        if 0 <= r < size and 0 <= c < size:
+            matrix[r][c] -= matrix[row][col] if matrix[r][c] > 0 else 0\
+
+alive_cells = [num for row in range(size) for num in matrix[row] if num > 0]
+
+print(f"Alive cells: {len(alive_cells)}")
+print(f"Sum: {sum(alive_cells)}")
+
+[print(*row) for row in matrix]
