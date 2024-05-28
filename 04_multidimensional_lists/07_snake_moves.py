d81421c14540b8be22a1d4f7ac592984a50970de
Monika Hristova <156455305+MonikaDalekova@users.noreply.github.com>
2024-05-28 17:20:47 +0300

    Update 07_snake_moves.py

--- 04_multidimensional_lists/07_snake_moves.py
+++ 04_multidimensional_lists/07_snake_moves.py
@@ -13,3 +13,23 @@ for row in range(rows):
         print(*[string_copy.popleft() for _ in range(cols)], sep='')
     else:
         print(*[string_copy.popleft() for _ in range(cols)][::-1], sep='')
+
+#2
+# rows, cols = [int(x) for x in input().split()]
+# word = input()
+# 
+# matrix = [['' for _ in range(cols)] for _ in range(rows)]
+# word_index = 0
+# word_length = len(word)
+# 
+# for row_index in range(rows):
+#     if row_index % 2 == 0:
+#         for col_index in range(cols):
+#             matrix[row_index][col_index] = word[word_index]
+#             word_index = (word_index + 1) % word_length # когато свършат буквите, да започне отначало
+#     else:
+#         for col_index in range(cols -1, -1, -1): # отзад напред
+#             matrix[row_index][col_index] = word[word_index]
+#             word_index = (word_index + 1) % word_length
+# 
+# [print(*row, sep='') for row in matrix]
